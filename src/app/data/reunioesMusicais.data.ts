55f7d2439a690bc7512b7d9bc9eeb6396ffd45b2
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-07-03 22:17:57 -0300

    fix(reunioes-musicais): Corrigindo informacoes

--- src/app/data/reunioesMusicais.data.ts
+++ src/app/data/reunioesMusicais.data.ts
@@ -15,8 +15,8 @@ export const reunioesMusicaisData = [
             "2024-06-02T19:30:00",
             "2024-12-01T19:30:00"
         ],
-        localidade: "Central de Lambari",
-        iframeMaps: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3699.7132197480837!2d-45.340406!3d-21.983967!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cb19b5a2de4b15%3A0xee221907c3ef6766!2sAv.%20Get%C3%BAlio%20Vargas%2C%201083%2C%20Lambari%20-%20MG%2C%2037480-000%2C%20Brasil!5e0!3m2!1spt-BR!2sus!4v1712848662969!5m2!1spt-BR!2sus",
+        localidade: "Campo Belo",
+        iframeMaps: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3699.9046315481287!2d-45.341782!3d-21.976623!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cb19ae098256d1%3A0x6a82a0cac42f453b!2sAv.%20drJos%C3%A9%20Nicolau%20mileo%2C%202001%2C%20Lambari%20-%20MG%2C%2037480-000%2C%20Brasil!5e0!3m2!1spt-BR!2sus!4v1712848087534!5m2!1spt-BR!2sus",
     },
     {
         nome: "Modelo 4 (Organistas)",
@@ -24,8 +24,8 @@ export const reunioesMusicaisData = [
         data: [
             "2024-10-21T19:30:00"
         ],
-        localidade: "Central de Lambari",
-        iframeMaps: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3699.7132197480837!2d-45.340406!3d-21.983967!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cb19b5a2de4b15%3A0xee221907c3ef6766!2sAv.%20Get%C3%BAlio%20Vargas%2C%201083%2C%20Lambari%20-%20MG%2C%2037480-000%2C%20Brasil!5e0!3m2!1spt-BR!2sus!4v1712848662969!5m2!1spt-BR!2sus",
+        localidade: "Campo Belo",
+        iframeMaps: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3699.9046315481287!2d-45.341782!3d-21.976623!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cb19ae098256d1%3A0x6a82a0cac42f453b!2sAv.%20drJos%C3%A9%20Nicolau%20mileo%2C%202001%2C%20Lambari%20-%20MG%2C%2037480-000%2C%20Brasil!5e0!3m2!1spt-BR!2sus!4v1712848087534!5m2!1spt-BR!2sus",
     },
     {
         nome: "Ensaio de Candidatos",
